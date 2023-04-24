328a5c85a20bb938e74d676de9668d125d581b85
rizalyala <rizalyala19@gmail.com>
2023-04-24 12:29:58 +0700

    A

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
+from tkinter import *
 import tkinter as tk
-from sidebar import Sidebar
+
+root = Tk()
+root.geometry('800x600')
+
+# membuat menu bar
+menubar = Menu(root)
+root.config(menu=menubar)
 
 
-root = tk.Tk()
-root.geometry("1080x600")
-root.title("Praktek SI")
+def page1():
+    # menampilkan halaman 1
+    halaman2.pack_forget()  # menghapus halaman 2 dari tampilan
+    halaman1.pack(fill=tk.BOTH, expand=True)
 
 
-# Main frame
-mainf = tk.Frame(root)
-mainf.pack(side='right', fill='both', expand=True)
+def page2():
+    # menampilkan halaman 2
+    halaman1.pack_forget()  # menghapus halaman 1 dari tampilan
+    halaman2.pack(fill=tk.BOTH, expand=True)
 
-sidebar = Sidebar(root, mainf)
+
+# membuat menu file
+
+# menambahkan menu file ke menu bar
+menubar.add_command(label="Home",  command=page1)
+
+# membuat menu help
+
+# menambahkan menu help ke menu bar
+menubar.add_command(label="Help",  command=page2)
+
+# membuat halaman 1
+halaman1 = tk.Frame(root)
+teks_halaman1 = tk.Label(halaman1, text="Ini adalah halaman 1")
+teks_halaman1.pack(padx=20, pady=20)
+
+# membuat halaman 2
+halaman2 = tk.Frame(root)
+teks_halaman2 = tk.Label(halaman2, text="Ini adalah halaman 2")
+teks_halaman2.pack(padx=20, pady=20)
+
 root.mainloop()
