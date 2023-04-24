4e357c17cfbcfebc01f0f58c9740c80daa720997
rizalyala <rizalyala19@gmail.com>
2023-04-24 08:57:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 
@@ -6,21 +7,32 @@ root.geometry("1280x720")
 
 root.title("Praktek SI")
 
+# Sidebar frame
 sidebar = tk.Frame(root, bg='gray', width=200, height=500)
 sidebar.pack(side='left', fill='y')
 
-# membuat tombol-tombol menu
-menu1_btn = tk.Button(sidebar, text='Menu 1')
-menu1_btn.pack(pady=10)
+# Main frame
+mainf = tk.Frame(root, bg='yellow')
+mainf.pack(side='right', fill='both', expand=True)
 
-menu2_btn = tk.Button(sidebar, text='Menu 2')
-menu2_btn.pack(pady=10)
+# membuat style untuk garis
+style = ttk.Style()
+style.configure('Treeview', rowheight=25, font=('Arial', 12), background='white',
+                foreground='black', fieldbackground='white', bordercolor='black', borderwidth=1)
+# membuat treeview
+tree = ttk.Treeview(mainf, columns=('col1', 'col2', 'col3'),
+                    show='headings', style='Treeview')
+tree.heading('col1', text='Kolom 1')
+tree.heading('col2', text='Kolom 2')
+tree.heading('col3', text='Kolom 3')
+tree.pack(side='left', fill='both', expand=True)
 
-menu3_btn = tk.Button(sidebar, text='Menu 3')
-menu3_btn.pack(pady=10)
-
-# membuat frame utama
-main_frame = tk.Frame(root, bg='white', width=500, height=500)
-main_frame.pack(side='right', fill='both', expand=True)
-
+# menambahkan data ke treeview
+data = [
+    ('Data 1', 'Data 2', 'Data 3'),
+    ('Data 4', 'Data 5', 'Data 6'),
+    ('Data 7', 'Data 8', 'Data 9')
+]
+for i, row in enumerate(data):
+    tree.insert(parent='', index=i, values=row)
 root.mainloop()
