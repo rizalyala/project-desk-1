d1b82852d3dc365af6814d51cf042ff0f45f5029
rizalyala <rizalyala19@gmail.com>
2023-05-02 17:06:41 +0700

    OKE

--- components/input_data.py
+++ components/input_data.py
@@ -1,10 +1,25 @@
 import tkinter as tk
+import pyrebase
 
 
 class InputDataFromFirebase(tk.Frame):
     def __init__(self, parent, res):
         super().__init__(parent)
 
+        # Database
+        firebaseConfig = {
+            "apiKey": "AIzaSyCDhnuglYOPe9B3N3eqBn_tvw2IOqxdQZc",
+            "authDomain": "sirs-01-3adfc.firebaseapp.com",
+            "databaseURL": "https://sirs-01-3adfc-default-rtdb.asia-southeast1.firebasedatabase.app",
+            "projectId": "sirs-01-3adfc",
+            "storageBucket": "sirs-01-3adfc.appspot.com",
+            "messagingSenderId": "1017194189692",
+            "appId": "1:1017194189692:web:76b902e8d1a65a5addcaf9"
+        }
+        firebase = pyrebase.initialize_app(firebaseConfig)
+        db = firebase.database()
+
+        # Window
         def input_window():
             window = tk.Toplevel()
             window.title("Tambah Data Pasien")
@@ -15,17 +30,23 @@ class InputDataFromFirebase(tk.Frame):
             label_.grid(row=0, column=1, columnspan=2)
 
             fields = ['Nama', 'Alamat', 'Usia', 'Ruang', 'Diagnosis']
-
+            entries = {}
             for i, field in enumerate(fields):
                 entry_label = tk.Label(window, text=field + " : ")
                 entry_label.grid(row=i+1, column=0, sticky="w", padx=10)
                 entry_ = tk.Entry(window, width=35)
                 entry_.grid(row=i+1, column=1, padx=10, pady=10)
 
+                entries[field] = entry_
+
             def submiting_data():
                 if submit_:
                     entry_.delete(0, tk.END)
+                    data = {}
+                    for field in fields:
+                        data[field] = entries[field].get()
 
+                db.child("pasien_datas").push(data)
             submit_ = tk.Button(
                 window, text="Add", border=0, bg="#EF5B0C", padx=10, fg="white", font=("Arial", 9, "bold"), command=submiting_data)
             submit_.grid(row=6, column=1, columnspan=2)
