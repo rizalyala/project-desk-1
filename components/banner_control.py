2ff466f846c0dbef4907e5d9ccff460befbc5bf8
rizalyala <rizalyala19@gmail.com>
2023-05-19 13:58:54 +0700

    Banner Update

--- components/banner_control.py
+++ components/banner_control.py
@@ -1,10 +1,24 @@
 from datetime import datetime
+import sqlite3
 import tkinter as tk
 
 
 class BannerControl(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
+
+        conn = sqlite3.connect('hospital.db')
+        cursor = conn.cursor()
+
+        q = "SELECT COUNT(*) FROM pasien"
+        cursor.execute(q)
+        result = cursor.fetchone()
+        jumlah_pas = result[0]
+
+        q = "SELECT COUNT(*) FROM dokter"
+        cursor.execute(q)
+        result = cursor.fetchone()
+        jumlah_doc = result[0]
 
         ban_container = tk.Frame(self, bg='#025464')
         ban_container.grid(row=0)
@@ -27,13 +41,13 @@ class BannerControl(tk.Frame):
 
         # Value
         ban_pas = tk.Label(
-            ban_container, text="0", bg='#025464', fg='white', font=("Arial", 10, "bold"))
+            ban_container, text=jumlah_pas, bg='#025464', fg='white', font=("Arial", 10, "bold"))
         ban_pas.grid(row=0, column=2, padx=5)
         ban_pas = tk.Label(
-            ban_container, text="0", bg='#025464', fg='white', font=("Arial", 10, "bold"))
+            ban_container, text="1000", bg='#025464', fg='white', font=("Arial", 10, "bold"))
         ban_pas.grid(row=1, column=2, padx=5)
         ban_pas = tk.Label(
-            ban_container, text="0", bg='#025464', fg='white', font=("Arial", 10, "bold"))
+            ban_container, text=jumlah_doc, bg='#025464', fg='white', font=("Arial", 10, "bold"))
         ban_pas.grid(row=2, column=2, padx=5)
 
         # Time
